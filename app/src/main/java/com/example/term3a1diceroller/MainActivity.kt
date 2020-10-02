b1a3c71fdd879075203b3fc3a34f133fa92b7fe1
Jacob Collins <hi8185@hotmail.com>
2020-10-02 17:18:42 -0400

    Buisness Layer completed, testing still needed

--- app/src/main/java/com/example/term3a1diceroller/MainActivity.kt
+++ app/src/main/java/com/example/term3a1diceroller/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.EditText
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,5 +12,25 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //initialization of certain values as requested in instructions
+        var total = 0;
+        result_et.setText(" ");
+
+        //when the roll button is clicked, creates an integer from 1-6, displays it,
+        //adds it to the total, and displays that
+        roll_btn.setOnClickListener() {
+            var roll = Random.nextInt(1, 6);
+            total += roll;
+            result_et.setText(roll);
+            total_txt.text = "Total= " + total.toString();
+        }
+
+        //when the reset button is clicked, sets all text fields that can be changed by the user
+        //back to their default
+        reset_btn.setOnClickListener() {
+            result_et.setText(" ");
+            total = 0;
+            total_txt.text = "Total= " + total.toString();
+        }
     }
 }
