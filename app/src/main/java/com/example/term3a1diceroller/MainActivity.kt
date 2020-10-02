845e08fe339d0b06058bffae5f53f44bb2375a72
Jacob Collins <hi8185@hotmail.com>
2020-10-02 16:14:55 -0400

    Presentation layer skeleton created, still needs polishing

--- app/src/main/java/com/example/term3a1diceroller/MainActivity.kt
+++ app/src/main/java/com/example/term3a1diceroller/MainActivity.kt
@@ -2,11 +2,14 @@ package com.example.term3a1diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+     
     }
 }
