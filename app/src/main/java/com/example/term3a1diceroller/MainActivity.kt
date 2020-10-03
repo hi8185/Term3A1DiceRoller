8bd2ae27a54995ac1149a5e240c07b215219ce53
Jacob Collins <hi8185@hotmail.com>
2020-10-02 21:20:33 -0400

    Testing completed, bugs fixed, beautification still needed

--- app/src/main/java/com/example/term3a1diceroller/MainActivity.kt
+++ app/src/main/java/com/example/term3a1diceroller/MainActivity.kt
@@ -2,35 +2,43 @@ package com.example.term3a1diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.EditText
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
+    //create class variable _total
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //initialization of certain values as requested in instructions
+        //set certain values initially as requested in instructions
         var total = 0;
-        result_et.setText(" ");
+
 
         //when the roll button is clicked, creates an integer from 1-6, displays it,
         //adds it to the total, and displays that
         roll_btn.setOnClickListener() {
-            var roll = Random.nextInt(1, 6);
+            val roll = Random.nextInt(1, 6);
+            val rollString = roll.toString();
             total += roll;
-            result_et.setText(roll);
-            total_txt.text = "Total= " + total.toString();
+            result_txt.setText(rollString);
+            totalDisplay(total)
         }
 
         //when the reset button is clicked, sets all text fields that can be changed by the user
         //back to their default
         reset_btn.setOnClickListener() {
-            result_et.setText(" ");
+            result_txt.setText(" ");
             total = 0;
-            total_txt.text = "Total= " + total.toString();
+            totalDisplay(total);
         }
     }
+
+    //displays the total on the screen in the desired text field
+    private fun totalDisplay(num: Int) {
+        var totalText = "Total= $num";
+        total_txt.text = totalText;
+    }
+
 }
